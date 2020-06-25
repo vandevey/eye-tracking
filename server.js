7f0f1a5349ff5debe31486d83e0d51ac36d7eb31
Yoan <yoan.vandevelde.isn@gmail.com>
2020-06-25 09:59:09 +0200

    Expo overview [WIP]

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 const express = require("express")
 const fs = require('fs');
 const app = express()
@@ -7,21 +5,24 @@ app.use(express.json())
 var array = [];
 var index = 1;
 
-app.use("/dist",express.static(__dirname + "/dist"))
-app.use("/css",express.static(__dirname + "/css"))
-app.use("/assets",express.static(__dirname + "/assets"))
+app.use("/dist", express.static(__dirname + "/dist"))
+app.use("/css", express.static(__dirname + "/css"))
+app.use("/assets", express.static(__dirname + "/assets"))
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/index.html")
 })
 
-app.get("/adds", function(req, res) {
+app.get("/adds", function (req, res) {
 	res.sendFile(__dirname + "/adds.html")
 })
+app.get("/expo", function (req, res) {
+	res.sendFile(__dirname + "/expo.html")
+})
 
-app.get("/results", function(req, res) {
+app.get("/results", function (req, res) {
 
-	fs.readFile('pointRecording.json', function(erreur, fichier) {
+	fs.readFile('pointRecording.json', function (erreur, fichier) {
 		var recordings;
 		recordings = JSON.parse(fichier);
 		console.log("recording: " + recordings);
@@ -31,16 +32,16 @@ app.get("/results", function(req, res) {
 })
 
 //Recording Json file for heatmap
-app.post('/data', function(req, res) {
-		array.push(req.body);
-		fs.writeFile('pointRecording.json', JSON.stringify(array), (err) => {
-			if (err) throw err;
-			index ++;
-			console.log('Data written to file');
-		});
+app.post('/data', function (req, res) {
+	array.push(req.body);
+	fs.writeFile('pointRecording.json', JSON.stringify(array), (err) => {
+		if (err) throw err;
+		index++;
+		console.log('Data written to file');
+	});
 });
 
-app.post('/deleteData', function(req, res) {
+app.post('/deleteData', function (req, res) {
 	console.log('Delete Data');
 	fs.unlinkSync('pointRecording.json');
 });
